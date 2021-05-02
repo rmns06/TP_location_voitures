c36a2c7c1817d2b6463230501ba425a5a0137f91
rmns06 <42636297+rmns06@users.noreply.github.com>
2021-05-02 22:51:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
-
-// header.style.background= `center/cover no-repeat url(${img[0]})`;
+// ajout slider auto pour le header
 let header = document.querySelector('#header');
 let cpt = 0;
 let img = ["./assets/fond.jpg","./assets/fond2.jpg","./assets/fond3.jpg"]
